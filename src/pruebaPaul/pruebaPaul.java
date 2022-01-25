ee16ba027452daf649b2788db638b742c1056f57
PaulEdouardFoulquier <97629223+PaulEdouardFoulquier@users.noreply.github.com>
2022-01-25 12:05:53 +0100

    Initial commit

---
+++ src/pruebaPaul/pruebaPaul.java
@@ -0,0 +1,16 @@
+package pruebaPaul;
+
+public class pruebaPaul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hola mundo");
+		
+		
+		
+		
+		
+	}
+
+}
